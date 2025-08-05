131437dd68ee1bb415ca6a456c4c1de1a015234b
isaquepamplona <isaquepamplonaa@gmail.com>
2025-08-05 20:56:22 -0300

    Add files via upload
    feat: Adiciona estrutura inicial do projeto C#

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CalculadoraWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Colaborativa Web");
+            // Integrante 1: Implementar menu e chamar as funções
+        }
+
+        // Integrante 1
+        public static double Adicao(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Integrante 2
+        // public static double Subtracao(double a, double b)
+        // {
+        //     // Implementar aqui
+        // }
+
+        // Integrante 3
+        // public static double Multiplicacao(double a, double b)
+        // {
+        //     // Implementar aqui
+        // }
+
+        // Integrante 4
+        // public static double Divisao(double a, double b)
+        // {
+        //     // Implementar aqui
+        // }
+    }
+}
+
